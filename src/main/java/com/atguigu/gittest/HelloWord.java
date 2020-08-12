4a67bc4b2eaa40f62941ad4f499f35cefa7e2cbf
doubleZ <1909039360@qq.com>
2020-08-12 20:26:07 +0800

    second commit

--- src/main/java/com/atguigu/gittest/HelloWord.java
+++ src/main/java/com/atguigu/gittest/HelloWord.java
@@ -8,5 +8,6 @@ package com.atguigu.gittest;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("你好 git");
+        System.out.println("你好 java");
     }
 }
